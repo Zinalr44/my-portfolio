a3be51b52dc5edae4a58610285ff76d8d62a8591
Zinalr44 <113049170+Zinalr44@users.noreply.github.com>
2024-09-23 15:23:35 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const modeToggle = document.getElementById('mode-toggle');
-modeToggle.addEventListener('change', () => {
-    if (modeToggle.checked) {
-        document.documentElement.setAttribute('data-theme', 'light');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-});
